04405a47f319daf8e8eee46a1a2512743f3efb2a
ccc96360 <ccc96360@naver.com>
2021-03-03 16:14:40 +0900

    Footer 생성 /src/assets/custom.scss생성 npm i node-sass sass-loader --save

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Icon from '@ant-design/icons';
-
+import {Col, Row} from "reactstrap"
+import '../../assets/custom.scss'
 function Footer() {
+    const thisYear = () => {
+        const year = new Date().getFullYear()
+        return year
+    }
     return (
-        <div style={{
-            height: '80px', display: 'flex',
-            flexDirection: 'column', alignItems: 'center',
-            justifyContent: 'center', fontSize:'1rem'
-        }}>
-           <p> Happy Coding  <Icon type="smile" /></p>
+        <div id="main-footer" className="text-center p-2">
+            <Row>
+                <Col>
+                    <p>Copyright &copy; <span>{thisYear()}</span></p>
+                </Col>
+            </Row>
         </div>
     )
 }
