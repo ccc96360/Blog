2645c01a5b3e5abd64277a29581f2600e1a51169
ccc96360 <ccc96360@naver.com>
2021-03-06 21:43:37 +0900

    게시글 작성 라우터완성 게시글 내용저장 게시글 카테고리 저장 카테고리 없을시 카테고리 목록 업데이트 moment 설치함

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,6 +4,7 @@ const path = require('path')
 let AWS = require('aws-sdk')
 const dotenv = require('dotenv')
 const multer = require('multer')
+const moment = require('moment')
 dotenv.config()
 
 
@@ -55,20 +56,61 @@ router.get('/', (req, res) =>{//req = request res = response
         }
     })
 })
-router.post('/',(req, res) =>{
-    const {title, owner, contents, fileUrl} = req.body
-    let qry = `insert into posts(title, owner, contents, imageUrl) values(?,?,?,?)`
-    params = [title, owner, contents, fileUrl||"https://source.unsplash.com/random/301x201"]
+router.post('/', uploadS3.none(),(req, res) =>{
+    const {title, owner, contents, fileUrl, category} = req.body
+    const date = moment().format("YYYY-MM-DD hh:mm:ss")
+    let qry = `insert into posts(title, owner, contents, imageUrl, date) values(?,?,?,?,?)`
+    let postUpload = false
+    let params = [title, owner, contents, fileUrl, date]
     db.query(qry, params, function(err, qryRes, fields){
         if(err){
             console.log("/api/post/ ERR!")
             console.log(err)
         }
         else{
-            res.status(200).json({
-                success: true
+            postUpload = true
+        }
+    })
+    let id;
+    db.query("select last_insert_id()",[], function(err, qryRes, fields){
+        let resJson = JSON.parse(JSON.stringify(qryRes))
+        console.log("lastID");
+        console.log(resJson);
+        id = resJson[0]["last_insert_id()"]
+        console.log(id);
+    })
+
+    qry = `select * from categories where categoryname = ?`
+    let categoryUpload = false
+    db.query(qry,[category], function(err, qryRes, fields){
+        if(qryRes == 0){
+            qry = 'insert into categories(categoryname) values(?)'
+            db.query(qry, [category], function(err,qryRes,fields){
+                if(err){
+                    console.log(err)
+                }
             })
         }
+        qry = `insert into posts_categories(postid, categoryname) values(?,?)`
+        params = [id, category]
+        db.query(qry, params, function(err, qryRes, fields){
+            if(err){
+                console.log(err)
+            }
+            else{
+                categoryUpload = true
+                if(postUpload && categoryUpload){
+                    res.status(200).json({
+                        uploadSuccess: true
+                    })
+                }
+                else{
+                    res.status(500).json({
+                        uploadSuccess: false
+                    })
+                }
+            }
+        })
     })
 })
 // /api/post/image
